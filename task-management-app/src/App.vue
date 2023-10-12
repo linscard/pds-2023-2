979f6bf13095a7bcea036b396fb7881c780a7e72
Thiago Campos <thiago.a.campos@hotmail.com>
2023-10-12 16:52:01 -0300

    Criando projeto e adicionando SideBarComponent

---
+++ task-management-app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <SideBarComponent></SideBarComponent>
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import SideBarComponent from './components/SideBarComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    SideBarComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ task-management-app/src/components/SideBarComponent.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <b-sidebar id="sidebar-1" title="Sidebar" no-header visible shadow>
+      <div class="pt-4 px-3 py-2">
+        <h2>Gest Tarefas</h2>
+        <div class="pt-3">
+          <b-button block variant="outline-secondary" class="w-100">Kanban</b-button>
+        </div>
+        <div class="pt-3">
+          <b-button block variant="outline-secondary" class="w-100">Add Task</b-button>
+        </div>
+        <div class="pt-3">
+          <b-button block variant="outline-secondary" class="w-100">Manage Categories</b-button>
+        </div>
+        <div class="pt-3">
+          <b-button block variant="outline-secondary" class="w-100">Add Colaborator</b-button>
+        </div>
+      </div>
+    </b-sidebar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
